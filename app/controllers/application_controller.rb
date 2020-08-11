0ce1b6f51fcf013d607ea3d72c03beb5fc8456d0
aecordova <ae_cordova@outlook.com>
2020-08-11 12:10:52 -0500

    Add User model and authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
   rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
   rescue_from ActiveRecord::RecordInvalid, with: :handle_invalid_record
+
+  before_action :authenticate_user!
 
   protected
 
