a7101a81b0576d99f3eca28d294825d6e410374f
aecordova <ae_cordova@outlook.com>
2020-08-10 21:44:03 -0500

    Add some tests for the rest of  models

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -1,4 +1,7 @@
 class Recipient < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+
   belongs_to :school
   has_many :orders
 
--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -1,28 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe Order, type: :model do
+RSpec.describe OrderItem, type: :model do
   describe 'associations' do
-    it { should belong_to(:school) }
-    it { should have_many(:items) }
-    it { should have_many(:recipients).through(:items) }
+    it { should belong_to(:order) }
+    it { should belong_to(:recipient) }
   end
 
   describe 'validations' do
-    it { should validate_presence_of(:school_id) }
+    it { should validate_presence_of(:recipient_id) }
     it { should validate_presence_of(:address) }
     it { should accept_nested_attributes_for(:items) }
     it { should define_enum_for(:status) }
 
     it 'Is not valid when order_item breaches daily gift limit' do
-
-    end
-    it 'is not valid when order_item breaches recipient limit' do
     end
 
-    it 'is not valid when ir doesnt follow proper order flow' do
-    end
-
-    it 'flags order for notification to user when ORDER_SHIPPED' do
+    it 'is not valid when order breaches recipient limit' do
     end
 
     it 'doesnt allow updates when status is ORDER_SHIPPED' do
@@ -33,7 +26,7 @@ RSpec.describe Order, type: :model do
   end
 
   describe 'methods' do
-    it 'Calculates the gift count of an order' do
+    it 'Calculates the gift count of an order' do 
     end
 
     it 'Returns an array of distinct recipient_ids' do
